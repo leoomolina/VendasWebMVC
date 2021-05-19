7af86b8c23ed96a0c0e3c8ef356caaca919878c7
Leonardo Molina <leomolina0501@hotmail.com>
2021-05-18 22:34:31 -0300

    Primeiro MVC (Departamentos)

---
+++ VendasWebMVC/Controllers/DepartamentosController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VendasWebMVC.Models;
+
+namespace VendasWebMVC.Controllers
+{
+    public class DepartamentosController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Departamento> list = new List<Departamento>();
+
+            list.Add(new Departamento { Id = 1, Name = "Eletrônicos" });
+            list.Add(new Departamento { Id = 2, Name = "Fashion" });
+
+            return View(list);
+        }
+    }
+}
--- VendasWebMVC/Controllers/HomeController.cs
+++ VendasWebMVC/Controllers/HomeController.cs
@@ -1,10 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using VendasWebMVC.Models;
+using VendasWebMVC.Models.ViewModels;
 
 namespace VendasWebMVC.Controllers
 {
