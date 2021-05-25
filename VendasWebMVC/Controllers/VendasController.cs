19181fb367d3236ed5f1bb4714c4236cbd8f4277
Leonardo Molina <leomolina0501@hotmail.com>
2021-05-24 23:25:12 -0300

    Criando telas de buscas

---
+++ VendasWebMVC/Controllers/VendasController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VendasWebMVC.Controllers
+{
+    public class VendasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult BuscaSimples()
+        {
+            return View();
+        }
+
+        public IActionResult BuscaAgrupada()
+        {
+            return View();
+        }
+    }
+}
---
+++ VendasWebMVC/Services/VendasService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VendasWebMVC.Models;
+
+namespace VendasWebMVC.Services
+{
+    public class VendasService
+    {
+        private readonly VendasWebMVCContext _context;
+
+        public DepartamentoService(VendasWebMVCContext context)
+        {
+            _context = context;
+        }
+    }
+}
